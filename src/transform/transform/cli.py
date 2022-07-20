6906e0cd8ce3b1540ca4cdd2830d0070830bda11
Chintanpatel90 <chintan.patel@techholding.co>
2022-07-20 14:25:38 +0530

    fix: changes

--- src/transform/transform/cli.py
+++ src/transform/transform/cli.py
@@ -10,9 +10,9 @@ def main():
 
     print("Arguments: " + str(args._))
     print("Replace this message by putting your code into "
-          "data_sdk.cli.main") 
+          "data_sdk.cli.main")
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main()) # pragma: no cover
+    sys.exit(main()) # pragma:no cover
