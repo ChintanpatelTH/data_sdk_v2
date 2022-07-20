b8a4a2a07c755727ba97b7f6f61bd21d00c36ed9
Chintanpatel90 <chintan.patel@techholding.co>
2022-07-20 18:52:53 +0530

    fix: changes4

--- src/normalize/normalize/cli.py
+++ src/normalize/normalize/cli.py
@@ -5,7 +5,7 @@ import sys
 def main():
     """Console script for data_sdk."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("_", nargs="*")
+    parser.add_argument("__", nargs="*")
     args = parser.parse_args()
 
     print("Argument: " + str(args._))
