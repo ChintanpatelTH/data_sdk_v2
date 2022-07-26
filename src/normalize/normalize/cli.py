f34185090a2e894518c9140892c84d23d3a839d1
Chintanpatel90 <chintan.patel@techholding.co>
2022-07-26 14:02:42 +0530

    feat: change workflow

--- src/normalize/normalize/cli.py
+++ src/normalize/normalize/cli.py
@@ -14,4 +14,4 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover11111223355
+    sys.exit(main())  # pragma: no cover1111122335544
--- src/transform/transform/cli.py
+++ src/transform/transform/cli.py
@@ -14,4 +14,4 @@ def main():
 
 
 if __name__ == "__main__":
-    sys.exit(main())  # pragma:no cover11
+    sys.exit(main())  # pragma:no cover1144
